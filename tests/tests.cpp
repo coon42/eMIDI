579659ce957293dbbbc9fe0a066af37e640b7b66
coon <coon@mailbox.org>
2018-01-18 23:08:14 +0100

    add more eMidi_open tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -6,7 +6,27 @@ extern "C" {
   #include "../src/midifile.h"
 }
 
-TEST_CASE( "eMidi_open tests", "[open]" ) {
-  REQUIRE( eMidi_open(NULL, NULL) == EMIDI_INVALID_HANDLE );
+TEST_CASE("eMidi_open tests", "[open]") {
+  SECTION("providing NULL for pMidiFile and NULL for pFileName") {
+    REQUIRE(eMidi_open(NULL, NULL) == EMIDI_INVALID_HANDLE);
+  }
+
+  SECTION("providing valid object for pMidiFile and NULL for pFileName") {
+    MidiFile midi;
+
+    REQUIRE(eMidi_open(&midi, NULL) == EMIDI_CANNOT_OPEN_FILE);
+  }
+
+  SECTION("providing valid object for pMidiFile and file name of invalid MIDI File") {
+    MidiFile midi;
+
+    REQUIRE(eMidi_open(&midi, "invalid.mid") == EMIDI_INVALID_MIDI_FILE);
+  }
+
+  SECTION("providing valid object for pMidiFile and file name of valid MIDI File") {
+    MidiFile midi;
+
+    REQUIRE(eMidi_open(&midi, "turkish_march0.mid") == EMIDI_OK);
+  }
 }
 
