0d1758723ad7df0bd0b89a7d8c2eab7316dc6b60
coon <coon@mailbox.org>
2018-02-14 00:41:52 +0100

    dump: print total runtime (is incorrect on vivaldi_spring0.mid?)

--- utils/dump.c
+++ utils/dump.c
@@ -1,6 +1,38 @@
 #include "midifile.h"
 #include "helpers.h"
 
+static void printTotalRuntime(MidiFile* pMidiFile) {
+  static const uint32_t c = 60000000;
+  uint32_t bpm = 120;
+  uint32_t uspqn = c / bpm;
+  uint32_t usTotal = 0;
+
+  MidiEvent e;
+  Error error;
+
+  do {
+    if(error = eMidi_readEvent(pMidiFile, &e)) {
+      printf("Error on reading event: [0x%02X] (Error %d: %s)\n",e.eventId, error,
+          eMidi_errorToStr(error));
+      return;
+    }
+
+    if(e.eventId == MIDI_EVENT_META) {
+      if(e.metaEventId == MIDI_SET_TEMPO)
+        uspqn = e.params.meta.setTempo.usPerQuarterNote;
+    }
+
+    usTotal += (e.deltaTime * uspqn) / pMidiFile->header.division.tqpn.TQPN;
+
+  } while (!(e.eventId == MIDI_EVENT_META && e.metaEventId == MIDI_END_OF_TRACK));
+
+  uint32_t m  = (usTotal / 60) / 1000000;
+  uint32_t s  = usTotal        / 1000000 - m * 60;
+  uint32_t ms = ((usTotal - m * 60 * 1000000 - s * 1000000) / 100 + 5) / 10;
+
+  printf("Duration: %02d:%02d:%03d\n", m, s, ms);
+}
+
 static void printMidiFileEvents(MidiFile* pMidiFile) {
   printf("Now reading MIDI events:\n");
   printf("========================\n\n");
@@ -38,9 +70,15 @@ int main(int argc, char* pArgv[]) {
   }
 
   printf("Midi file '%s' opened successfully!\n", pMidiFileName);
+
   eMidi_printFileInfo(&midi);
   printMidiFileEvents(&midi);
+  eMidi_close(&midi);
 
+  printf("======\n");
+
+  eMidi_open(&midi, pMidiFileName);
+  printTotalRuntime(&midi);
   eMidi_close(&midi);
 
   return 0;
