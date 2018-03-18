7b8d6172be58c3b000743b9f4570c94b7e60d38a
coon <coon@mailbox.org>
2018-03-18 03:15:58 +0100

    also add pre declaration for eMidi_drumToStr

--- src/helpers.h
+++ src/helpers.h
@@ -5,6 +5,7 @@ const char* eMidi_errorToStr(Error error);
 const char* eMidi_eventToStr(uint8_t eventCode);
 const char* eMidi_metaEventToStr(int metaEventId);
 const char* eMidi_numberToNote(uint8_t midiNote);
+const char* eMidi_drumToStr(uint8_t midiNote);
 const char* eMidi_programToStr(uint8_t programNo);
 
 Error eMidi_printMidiEvent(const MidiEvent* e);
