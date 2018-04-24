2d42ea18759d1879d5610658ed03c803155cbef6
coon <coon@mailbox.org>
2018-04-25 00:33:59 +0200

    midi2array: generate filesize at begin of array

--- utils/midi2array.c
+++ utils/midi2array.c
@@ -1,6 +1,7 @@
 #define _DEFAULT_SOURCE
 #include <unistd.h>
 
+#include <stdlib.h>
 #include <string.h>
 #include "midifile.h"
 #include "helpers.h"
@@ -22,23 +23,38 @@ static void writeStr(FILE* pSourceFile, const char* pStr) {
   eMidi_fwrite(pStr, 1, strlen(pStr), pSourceFile);
 }
 
+static uint32_t prvGetFileSize(FILE* p) {
+  eMidi_fseek(p, 0, SEEK_END);
+  uint32_t fileSize = eMidi_ftell(p);
+  eMidi_fseek(p, 0, SEEK_SET);
+
+  return fileSize;
+}
+
 static void writeArray(FILE* pMidiFile, FILE* pSourceFile) {
+  uint32_t fileSize = prvGetFileSize(pMidiFile);
+  char* p = malloc(fileSize + sizeof(uint32_t));
+  memcpy(p, &fileSize, sizeof(uint32_t));
+
+  eMidi_fread(&p[4], 1, fileSize, pMidiFile);
+
   writeStr(pSourceFile, "const unsigned char pMidi[] = {");
 
-  unsigned char b;
-
-  for(int i = 0; eMidi_fread(&b, 1, 1, pMidiFile) == 1; ++i) {
+  for(int i = 0; i < fileSize + sizeof(uint32_t); ++i) {
     if(i % 20 == 0) // limit text to about 100 columns
       writeStr(pSourceFile, "\n  ");
 
     char pNum[32];
-    snprintf(pNum, sizeof(pNum), "0x%02X,", (int)b);
+    snprintf(pNum, sizeof(pNum), "0x%02X,", (unsigned char)p[i]);
 
     writeStr(pSourceFile, pNum);
   }
 
   writeStr(pSourceFile, "\n");
   writeStr(pSourceFile, "};\n");
+
+  if(p)
+    free(p);
 }
 
 int main(int argc, char* pArgv[]) {
