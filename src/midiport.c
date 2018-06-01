5f795600c3179be394ce908b9cbdf660c1a2dcc8
coon <coon@c-base.org>
2018-06-01 21:01:58 +0200

    implement eMidi_enumInPorts() function (windows)

--- src/midiport.c
+++ src/midiport.c
@@ -3,28 +3,49 @@
 #include <windows.h> // TODO: move to windows HAL implementation
 #include <string.h>
 
-Error eMidi_enumOutPorts(uint32_t index, MidiPortInfo* pPortInfo) {
-  int numDevs = midiOutGetNumDevs();
+typedef enum PortType {
+  IN_PORT,
+  OUT_PORT
+} PortType;
+
+static Error prvEnumPorts(uint32_t index, MidiPortInfo* pPortInfo, PortType portType) {
+  int numDevs = portType == IN_PORT ? midiInGetNumDevs() : midiOutGetNumDevs();
 
   if (index >= numDevs)
     return EMIDI_END_OF_PORTS;
 
-  MIDIOUTCAPS caps;
-  midiOutGetDevCaps(index, &caps, sizeof(MIDIOUTCAPS));
+  MIDIINCAPS inCaps;
+  MIDIOUTCAPS outCaps;
+  const wchar_t* pName;
+
+  if (portType == IN_PORT) {
+    midiInGetDevCaps(index, &inCaps, sizeof(MIDIINCAPS));
+    pName = inCaps.szPname;
+  }
+  else {
+    midiOutGetDevCaps(index, &outCaps, sizeof(MIDIOUTCAPS));
+    pName = outCaps.szPname;
+  }
 
   pPortInfo->id = index;
 
 #if defined(UNICODE) || defined( _UNICODE )
   memset(pPortInfo->pName, 0, sizeof(pPortInfo->pName));
 
-  int length = WideCharToMultiByte(CP_UTF8, 0, caps.szPname, -1, NULL, 0, NULL, NULL) - 1;
-
-  length = WideCharToMultiByte(CP_UTF8, 0, caps.szPname, (int)(wcslen(caps.szPname)),
-      pPortInfo->pName, length, NULL, NULL);
+  int length = WideCharToMultiByte(CP_UTF8, 0, pName, -1, NULL, 0, NULL, NULL) - 1;
+  WideCharToMultiByte(CP_UTF8, 0, pName, (int)(wcslen(pName)), pPortInfo->pName, length, NULL, NULL);
 
 #else
-  strncpy(pPortInfo->pName, caps.szPname, sizeof(pPortInfo->pName));
+  strncpy(pPortInfo->pName, pName, sizeof(pPortInfo->pName));
 #endif
 
   return EMIDI_OK;
+}
+
+Error eMidi_enumInPorts(uint32_t index, MidiPortInfo* pPortInfo) {
+  return prvEnumPorts(index, pPortInfo, IN_PORT);
+}
+
+Error eMidi_enumOutPorts(uint32_t index, MidiPortInfo* pPortInfo) {
+  return prvEnumPorts(index, pPortInfo, OUT_PORT);
 }
--- src/midiport.h
+++ src/midiport.h
@@ -8,6 +8,7 @@ typedef struct MidiPortInfo {
   char pName[64];
 } MidiPortInfo;
 
+Error eMidi_enumInPorts(uint32_t index, MidiPortInfo* pPortInfo);
 Error eMidi_enumOutPorts(uint32_t index, MidiPortInfo* pPortInfo);
 
 #endif // !_MIDI_PORT
