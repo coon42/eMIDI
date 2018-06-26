5c672f1b911c921c37905ff4d7f34641019b72f0
coon <coon@mailbox.org>
2018-06-26 22:38:13 +0200

    utils: add sample for create API

---
+++ utils/create.c
@@ -0,0 +1,24 @@
+#include "helpers.h"
+
+int main(int argc, char* pArgv[]) {
+  Error error;
+  MidiFile midi;
+
+  if (error = eMidi_create(&midi)) {
+    eMidi_printError(error);
+    return 1;
+  }
+
+  if (error = eMidi_save(&midi, "example.mid")) {
+    eMidi_printError(error);
+    return 2;
+  }
+
+  if(error = eMidi_close(&midi)) {
+    eMidi_printError(error);
+    return error;
+  }
+
+  return 0;
+}
+
