c67087fc0c0ff36b5cca9d78a8de90e5cded47ce
coon <coon@mailbox.org>
2019-04-02 21:08:40 +0200

    cosmetics

--- src/midiplayer.c
+++ src/midiplayer.c
@@ -8,7 +8,7 @@ static void dispatchEvent(const MidiPlayer* pPlayer) {
 static Error reload(MidiPlayer* pPlayer) {
   Error error;
 
-  if(error = eMidi_readEvent(&pPlayer->midi, &pPlayer->event))
+  if (error = eMidi_readEvent(&pPlayer->midi, &pPlayer->event))
     return error;
 
   pPlayer->lastReloadTimeUs = eMidi_timeUs();
@@ -17,14 +17,14 @@ static Error reload(MidiPlayer* pPlayer) {
 }
 
 static Error shoot(MidiPlayer* pPlayer) {
-  if(pPlayer->event.eventId == MIDI_EVENT_META) {
-    if(pPlayer->event.metaEventId == MIDI_SET_TEMPO)
+  if (pPlayer->event.eventId == MIDI_EVENT_META) {
+    if (pPlayer->event.metaEventId == MIDI_SET_TEMPO)
       pPlayer->uspqn = pPlayer->event.params.msg.meta.setTempo.usPerQuarterNote;
   }
 
   dispatchEvent(pPlayer);
 
-  if(pPlayer->event.eventId == MIDI_EVENT_META && pPlayer->event.metaEventId == MIDI_END_OF_TRACK)
+  if (pPlayer->event.eventId == MIDI_EVENT_META && pPlayer->event.metaEventId == MIDI_END_OF_TRACK)
     return EMIDI_OK_END_OF_FILE;
 
   return EMIDI_OK;
@@ -34,10 +34,10 @@ Error eMidi_openPlayer(MidiPlayer* pPlayer, const char* pFileName,
     UserEventCallback_t pUserEventCallback, void* pContext) {
   Error error;
 
-  if(error = eMidi_open(&pPlayer->midi, pFileName))
+  if (error = eMidi_open(&pPlayer->midi, pFileName))
     return error;
 
-  if(error = reload(pPlayer))
+  if (error = reload(pPlayer))
     return error;
 
   static const uint32_t c = 60000000;
@@ -66,13 +66,13 @@ Error eMidi_playerTick(MidiPlayer* pPlayer) {
   uint32_t usToWait = (pPlayer->event.deltaTime * pPlayer->uspqn) / tqpn;
   uint32_t usPassed = eMidi_timeUs() - pPlayer->lastReloadTimeUs;
 
-  if(usPassed < usToWait)
+  if (usPassed < usToWait)
     return EMIDI_OK;
 
-  if(error = shoot(pPlayer))
+  if (error = shoot(pPlayer))
     return error;
 
-  if(error = reload(pPlayer))
+  if (error = reload(pPlayer))
     return error;
 
   return EMIDI_OK;
