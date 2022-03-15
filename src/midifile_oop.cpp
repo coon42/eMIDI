a4736085e35a6f291fb5a9a6fe391558b029660f
coon <coon@mailbox.org>
2022-03-15 13:54:32 +0100

    midifile_oop: add EmNoteOnEvent

--- src/midifile_oop.cpp
+++ src/midifile_oop.cpp
@@ -1,2 +1,9 @@
 #include "midifile_oop.h"
 
+//-------------------------------------------------------------------------------------------------
+// EmNoteOnEvent
+//-------------------------------------------------------------------------------------------------
+
+Error EmNoteOnEvent::write(uint32_t deltaTime) const {
+  return eMidi_writeNoteOnEvent(pMidiFile_, deltaTime, channel(), note(), velocity());
+}
--- src/midifile_oop.h
+++ src/midifile_oop.h
@@ -42,4 +42,38 @@ public:
     return EMIDI_OK;
   }
 };
+
+//-------------------------------------------------------------------------------------------------
+// EmNoteEvent
+//-------------------------------------------------------------------------------------------------
+
+class EmNoteEvent : public EmMidiEvent {
+public:
+  EmNoteEvent(MidiFile* pMidiFile, uint8_t eventId, uint32_t absoluteTick, uint8_t channel, uint8_t note,
+      uint8_t velocity)
+    : EmMidiEvent(pMidiFile, eventId, absoluteTick), channel_(channel), note_(note), velocity_(velocity) {}
+  
+  Error write(uint32_t deltaTime) const override = 0;
+
+  uint8_t channel()  const                    { return channel_; }
+  uint8_t note()     const                    { return note_; }
+  uint8_t velocity() const                    { return velocity_; }
+
+private:
+  const uint8_t channel_;
+  const uint8_t note_;
+  const uint8_t velocity_;
+};
+
+//-------------------------------------------------------------------------------------------------
+// EmNoteOnEvent
+//-------------------------------------------------------------------------------------------------
+
+class EmNoteOnEvent : public EmNoteEvent {
+public:
+  EmNoteOnEvent(MidiFile* pMidiFile, uint32_t absoluteTick, uint8_t channel, uint8_t note, uint8_t velocity)
+      : EmNoteEvent(pMidiFile, MIDI_EVENT_NOTE_ON, absoluteTick, channel, note, velocity) {}
+  
+  Error write(uint32_t deltaTime) const final;
+};
 #endif // MIDIFILE_OOP_H
