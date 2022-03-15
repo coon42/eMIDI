c64dea08389ffaf2545a08caf486904e0c3c8fb2
coon <coon@mailbox.org>
2022-03-15 13:53:29 +0100

    midifile_oop.h: add EmMidiEvent

--- src/midifile_oop.h
+++ src/midifile_oop.h
@@ -5,4 +5,26 @@ extern "C" {
 #include "midifile.h"
 }
 
+//-------------------------------------------------------------------------------------------------
+// EmMidiEvent
+//-------------------------------------------------------------------------------------------------
+
+class EmMidiEvent {
+public:
+  EmMidiEvent(MidiFile* pMidiFile, uint8_t eventId, uint32_t absoluteTick)
+      : pMidiFile_(pMidiFile), eventId_(eventId), absoluteTick_(absoluteTick) {}
+
+  virtual ~EmMidiEvent() = 0                  { };
+  virtual Error write(uint32_t deltaTime) const = 0;
+
+  uint8_t eventId() const                     { return eventId_; }
+  uint32_t absoluteTick() const               { return absoluteTick_; }
+
+protected:
+  MidiFile* pMidiFile_{nullptr};
+
+private:
+  const uint8_t eventId_;
+  const uint32_t absoluteTick_;
+};
 #endif // MIDIFILE_OOP_H
